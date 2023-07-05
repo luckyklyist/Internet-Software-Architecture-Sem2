fa5864e8bc737dd6836cd2b1269bb26e4f1f1600
luckyklyist <puspaanupam@gmail.com>
2023-07-05 10:38:48 +0545

    added the obj

---
+++ week5/obj.js
@@ -0,0 +1,29 @@
+// const hdfhdsjfh={
+//     name:"ramam",
+//     age:10,
+//     birthPlace:"Nepal",
+//     info:{
+//         bio:"hero"
+//     },
+//     hobby:["effsf","dfdf"]
+// }
+
+// // const {info:{bio}}=names;
+
+// const {info:{bio}}=names;
+
+// console.log(names.birthPlace);
+
+// console.log(names.info.bio);
+
+// // info 
+
+// console.log(bio);
+
+
+const obj={
+    name:"rama",
+    rama:function(){console.log(this.name)}
+}
+
+obj.rama()
